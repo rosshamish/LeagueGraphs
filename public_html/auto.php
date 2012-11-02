28f3214c631087974fefb6a3526ebb500f0be038
Ross Anderson <ross.anderson87@gmail.com>
2012-11-01 20:32:05 -0600

    Add some rough safety to auto.php
    - it sets a text file saying that it's already being used
    - to reset it and start a new browser page, you have to reset the
    auto.txt file on the server MANUALLY
    - this is only because window.onbeforeunload is being stupid

--- public_html/auto.php
+++ public_html/auto.php
@@ -1,8 +1,23 @@
-<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
-<script type="text/javascript">
+<html>
+    <head>
+<?php
+    $myFile = "auto.txt";
+    $fh = fopen($myFile, 'r') or die("can't open file for reading");
+    $readData = fread($fh, 6);
+    if ($readData == 'in use') {
+        window.close();
+    } else {
+        fclose($fh);
+        $fh = fopen($myFile, 'w') or die("can't open file");
+        $stringData = "in use";
+        fwrite($fh, $stringData);
+        fclose($fh);
+    }
+?>
 
 <?php
-
+    // it is running, yo
+    
     require_once('sensitive_data.php');
     mysql_connect($host, $username, $password);
     mysql_selectdb($database);
@@ -24,18 +39,16 @@
     mysql_close();
 ?>
 
-
+        
+        
+<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+<script type="text/javascript">
+    // set up the "i'm done using this now goddammit" notice
+window.onbeforeunload = function yaya() {
+    return "haha";
+}
 
 $(document).ready(function() {
-
-function print_arr(arr, arrname) {
-    var debug_string = "<br>" + "== " + arrname + " ==";
-    for (var i=0; i < arr.length; i++) {
-        debug_string += "<br> i: " + i + ", val: " + arr[i];
-    }
-    debug_string += "<br>======<br><br>";
-    //$("#debug").append(debug_string);
-}
 
 // each call to this function is worth 2 api calls
 function updateNext() {
@@ -45,8 +58,10 @@ function updateNext() {
     url: "lol_processuser.php",
     data: "summonerName=" + names_arr[count],
     success: function(phpdata) {
+        var games = phpdata.split(":");
+        games = games[0];
         document.write('--<br>');
-        document.write(names_arr[count] + '(player #' + count + ') updated!<br>');
+        document.write(names_arr[count] + '(player #' + count + ') updated! ' + games + ' new games grabbed.<br>');
         count++;
         if (count >= names_arr.length) {
             count = 0;
@@ -55,8 +70,22 @@ function updateNext() {
     });
     
 };
+
+function updateTimer() {
+    var minutes = Math.floor(timer / (1000 * 60));
+    var seconds = (timer - minutes*60*1000) / 1000;
+    $("#updateTimer").html("Next update in " + minutes + ":" + seconds);
+    timer -= 1000;
+    if (timer <= 0) {
+        timer = interval;
+    }
+}
 var names_arr = <?php echo $arr_json ?>;
 var count = 0;
+var timer = 0;
+document.write("<h1>Database auto-updater</h1><br>");
+document.write("<h2></h2>");
+document.write("<div id='updateTimer'></div>");
 
 var tenminutes = 1000 * 60 * 10;
 var thirtyminutes = 1000 * 60 * 30;
@@ -64,10 +93,15 @@ var hour = 1000 * 60 * 60;
 var interval = hour;
 updateNext(); // call it the first time
 setInterval(updateNext, interval); // set up the interval
+timer = interval;
+setInterval(updateTimer, 1000); // set up the timer
 
-document.write("<h2>Database auto-updater</h2><br>");
-document.write("...working...<br><h1>Currently doing one update every " + interval / (1000 * 60) + " minutes</h1>");
+$("h2").html("Currently set to one update every " + interval / (1000 * 60) + " minutes");
+
 
 });
 
 </script>
+    </head>
+
+</html>

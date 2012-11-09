07a79933983350e0f48d6219eb9bb22c3c47d60f
Ross Anderson <ross.anderson87@gmail.com>
2012-11-09 13:24:35 -0700

    Almost done graph filtering 2
    - just need to add new rects instead of overwriting the old ones
    yay :3

--- public_html/dev/grapher.js
+++ public_html/dev/grapher.js
@@ -40,20 +40,35 @@ function get_graph(summoner_name, x_field, y_field, champId) {
                 y_field : y_field,
                 champId : champId},
         dataType: "json",
-        success: function(phpdata) {            
+        success: function(phpdata) {
+            //console.log('---');
+            //console.log(phpdata);
+            //console.log('---');
+            
             var data = phpdata;
             var x_array = [];
             var y_array = [];
-            
-            for (var i=0; i< data.length; i++) {
-                var xval = data[i].x;
-                var yval_arr = [];
-                for (var j=0; j < data[i].y.length; j++) {
-                  yval_arr.push(data[i].y[j]);
+            /* format: [{"x":"178992375","y":["2","2"]},{"x":"179015423","y":["15","1"]}] */
+            // we'll have to build more than one y_array (y_array will be an array of arrays)
+            for (var y_coord=0; y_coord< data[0].y.length; y_coord++) {
+                var yval_arr = []; // clear the y values array
+                var xval_arr = [];
+                for (var x_coord=0; x_coord< data.length; x_coord++) {
+                    var xval = data[x_coord].x;
+                    
+                    yval_arr.push(data[x_coord].y[y_coord]);
+                    xval_arr.push(data[x_coord].x);
                 }
-                x_array.push(xval);
-                y_array.push(yval_arr);
+                y_array.push(yval_arr); // push the y values array, then clear it ^^ up there
+                x_array.push(xval_arr); // push the x values array, then clear it ^^ up there
             }
+            //console.log('===');
+            //for (var i=0; i<y_array.length; i++) {
+            //    console.log("at finish, x_array[" + i + "]: " + x_array[i]);
+            //    console.log("at finish, y_array[" + i + "]: " + y_array[i]);
+            //    console.log('--');
+            //}
+            //console.log('===');
             
             /** deal with "time" being the desired x value */
             // if time is the desired x-axis (i.e. gameId)
@@ -62,44 +77,60 @@ function get_graph(summoner_name, x_field, y_field, champId) {
                 // their values to reasonable integers
                 for (var i=0; i < x_array.length; i++) {
                     // set x values to 0 through the length
-                    x_array[i] = i;
+                    for (var j=0; j < x_array[i].length; j++) {
+                        x_array[i][j]= j;
+                    }  
                 }
             }
+            console.log('===');
+            for (var i=0; i < x_array.length; i++) {
+                for (var j=0; j < x_array[i].length; j++) {
+                    console.log('x_array[' + i + '][' + j + ']: ' + x_array[i][j]);
+                }
+            }
+            console.log('===');
+            console.log('===');
+            for (var i=0; i < y_array.length; i++) {
+                for (var j=0; j < y_array[i].length; j++) {
+                    console.log('y_array[' + i + '][' + j + ']: ' + y_array[i][j]);
+                }
+                console.log('y_array['+i+'].length: ' + y_array[i].length);
+            }
+            console.log('===');
+            var data_length = y_array[0].length;
+            console.log('data_length: ' + data_length);
             
             /** domain and range */
             padding = width / x_array.length;
             // set domain
-            var xmin = Math.min.apply(Math, x_array);
-            var xmax = Math.max.apply(Math, x_array);
+            var xmin = Math.min.apply(Math, x_array[0]);
+            var xmax = Math.max.apply(Math, x_array[0]);
             var xScale = d3.scale.linear()
                           .domain([xmin, xmax])
                           .range([0, width-padding]);
             
             /** this is the part that needs to be done separately for each filter */
-            for (var idx=0; idx < y_array[0].length; idx++) {
-                var cur_y_arr = y_array[0];
-                console.log('cur_y_arr: ' + cur_y_arr);
-                return;
+            for (var cur_field=0; cur_field < data_length; cur_field++) { // fix this TODO
                 // set range
-                var ymin = Math.min.apply(Math, cur_y_arr);
-                var ymax = Math.max.apply(Math, cur_y_arr); 
+                var ymin = Math.min.apply(Math, y_array[cur_field]);
+                var ymax = Math.max.apply(Math, y_array[cur_field]); 
                 var yScale = d3.scale.linear()
                               .domain([ymin, ymax])
                               .range([0, height])
                 
                 // build the data
                 data = [];
-                for (var i=0; i < x_array.length; i++) {
-                    data.push({'x': x_array[i], 'y': cur_y_arr[i]});
+                for (var idx=0; idx < data_length; idx++) {
+                    data.push({'x': x_array[cur_field][idx], 'y': y_array[cur_field][idx]});
                 }
                 
                 /* Do the graph */
                 // Empty the old graph
                 $("#graph").empty();
-                $("#graph").hide();
+                //$("#graph").hide();
                 
                 // Build the plot.
-                var y_axis = $.parseJSON($.cookie('filters'))[idx];
+                var y_axis = $.parseJSON($.cookie('filters'))[cur_field];
                 if (x_field == 'gameId') {
                     var x_axis = 'time';
                     var title = y_axis + " per game for " + summoner_name;
@@ -132,7 +163,7 @@ function get_graph(summoner_name, x_field, y_field, champId) {
                         return height - yScale(d.y);
                         })
                     .attr("fill", function(d) {
-                        return d3.hsl(249, 0.10 * idx, d.y / (ymax*2));
+                        return d3.hsl(100 * cur_field, 0.85, d.y / (ymax*2));
                         });
                     
                 svg.selectAll("text")
@@ -155,7 +186,6 @@ function get_graph(summoner_name, x_field, y_field, champId) {
                     .attr("y", function(d) {
                         return height - yScale(d.y) + 15;
                     });
-            
             }
             // now show the graph, since we're all done making it
             $("#graph").show();
